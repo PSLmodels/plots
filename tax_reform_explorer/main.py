6762d849e5201cfb3b5101344dac697aab0f0c25
bcollins <bcollins@continuum.io>
2016-05-09 13:30:24 -0500

    integrated real data

--- tax_reform_explorer/main.py
+++ tax_reform_explorer/main.py
@@ -1,10 +1,13 @@
 '''
 Tax Brain Widget
 '''
+import glob
 import os
 import pandas as pd
 import numpy as np
 from numpy.random import randint
+
+from pdb import set_trace
 
 from jinja2 import Environment, FileSystemLoader
 
@@ -18,9 +21,6 @@ from styles import (PLOT_FORMATS,
                     RED,
                     BLUE)
 
-plot_width = 400
-plot_height = 250
-
 def output_page(output_path, **kwargs):
     here = os.path.dirname(os.path.abspath(__file__))
     j2_env = Environment(loader=FileSystemLoader(here), trim_blocks=True)
@@ -28,52 +28,45 @@ def output_page(output_path, **kwargs):
     with open(output_path, 'w') as output_file:
         output_file.write(content)
 
-# ------------------------
-def _create_mock_data():
-
-    def _random_source(k):
-        df = pd.DataFrame()
-        df['average_tax'] = np.random.normal(50000, 10000, 100)
-        df['x'] = df.apply(lambda r: randint(0, 100), axis=1)
-        df['x1'] = df.apply(lambda r: randint(0, 100), axis=1)
-        df['income_percentile'] = pd.Series(list(range(100)))
-
-        # line plot
-        baseline_itemized = pd.Series([randint(10, 75) for r in range(100)])
-        baseline_itemized.sort()
-        df['baseline_itemized'] = baseline_itemized.values
-
-        reform_itemized = pd.Series([randint(10, 75) for r in range(100)])
-        reform_itemized.sort()
-        df['reform_itemized'] = reform_itemized.values
-
-        # bar plot
-        baseline_bars = pd.Series([randint(0, 13) for r in range(100)])
-        df['baseline_bars'] = baseline_bars + .125
-
-        reform_bars = pd.Series([randint(0, 13) for r in range(100)])
-        df['reform_bars'] = reform_bars - .125
-
-        return ColumnDataSource(df)
-
-    groups = []
-    for i in range(3):
-        for j in range(3):
-            for k in range(3):
-                groups.append((str(i), str(j), str(k)))
-
-    groups = ['ds_' + ''.join(g) for g in groups]
-    column_data_sources = map(_random_source, list(range(len(groups))))
-    return dict(zip(groups, column_data_sources))
-
-# ------------------------
-
 def get_data_sources():
-    # TODO: replace with actual data...
-    return _create_mock_data()
 
-data_sources = get_data_sources()
+    tax_average_bin_cats = reversed(['(-100000000000000, 0]',
+                                      '(0, 9999]',
+                                      '(9999, 19999]',
+                                      '(19999, 29999]',
+                                      '(29999, 39999]',
+                                      '(39999, 49999]',
+                                      '(49999, 74999]',
+                                      '(74999, 99999]',
+                                      '(99999, 199999]',
+                                      '(199999, 499999]',
+                                      '(499999, 1000000]',
+                                      '(1000000, 100000000000000]'])
+    tax_average_bin_cats = list(tax_average_bin_cats)
+    files = glob.glob('ds_*_*.csv')
+    line_sources = {}
+    bar_sources = {}
+    for f in files:
+        ds_id = os.path.splitext(f)[0]
+        df = pd.read_csv(f)
 
+        if '_data' in ds_id:
+            df['base'] = df['base'] * 100
+            df['reform'] = df['reform'] * 100
+            line_sources[ds_id] = ColumnDataSource(df)
+
+        elif '_diff' in ds_id:
+            df['bins'] = df['bins'].map(lambda r:tax_average_bin_cats.index(r))
+            df['reform_bins'] = df['bins'] - .125
+            df['base_bins'] = df['bins'] + .125
+            bar_sources[ds_id] = ColumnDataSource(df)
+
+    return line_sources, bar_sources
+
+plot_width = 400
+plot_height = 250
+
+line_sources, bar_sources = get_data_sources()
 tax_average_bin_names = reversed(['Less than 10',
                                   '10 - 20',
                                   '20 - 30',
@@ -91,9 +84,8 @@ logo_url = 'https://avatars0.githubusercontent.com/u/8301070?v=3&s=200'
 logo_source = ColumnDataSource(dict(url=[logo_url], x=[17], y=[.8], w=[100], h=[12]))
 logo_image = ImageURL(url="url", x="x", y="y", global_alpha=.05, anchor="bottom_left")
 
-source = ColumnDataSource(data_sources.values()[0].data)
-
 # create line plot --------------------------------------------------
+lines_source = ColumnDataSource(line_sources.values()[0].data)
 lines = Plot(plot_width=plot_width,
              plot_height=plot_height,
              x_range=Range1d(0, 100),
@@ -102,47 +94,49 @@ lines = Plot(plot_width=plot_width,
 
 lines.add_glyph(logo_source, logo_image)
 
-lines.add_glyph(source,
-                Line(x='income_percentile',
-                     y='baseline_itemized',
-                     line_color=BLUE,
-                     line_width=2,
-                     line_alpha=0.8))
+line_base_renderer = lines.add_glyph(lines_source,
+                                Line(x='index',
+                                     y='base',
+                                     line_color=BLUE,
+                                     line_width=2,
+                                     line_alpha=0.8))
 
-lines.add_glyph(source,
-                Line(x='income_percentile',
-                     y='reform_itemized',
-                     line_color=RED,
-                     line_width=2,
-                     line_alpha=.8))
+line_reform_renderer = lines.add_glyph(lines_source,
+                                       Line(x='index',
+                                            y='reform',
+                                            line_color=RED,
+                                            line_width=2,
+                                            line_alpha=.8))
+
 
 lines.add_layout(LinearAxis(axis_label="Percentiles of Income", **AXIS_FORMATS), 'below')
 lines.add_layout(LinearAxis(axis_label="% Itemizing", **AXIS_FORMATS), 'left')
 
 # create bar plot -------------------------------------
+bars_source = ColumnDataSource(bar_sources.values()[0].data)
 bars = Plot(plot_width=plot_width,
             plot_height=plot_height,
             title='Average Tax',
-            x_range=Range1d(0, 100),
+            x_range=Range1d(0, 100000),
             y_range=FactorRange(*tax_average_bin_names),
             **PLOT_FORMATS)
 
 bars.add_glyph(logo_source, logo_image)
 
-bars.add_glyph(source,
-               Rect(x='x',
-                    y='baseline_bars',
-                    height=0.2,
-                    width=100,
-                    fill_color=BLUE,
-                    fill_alpha=0.8,
-                    line_color=None))
+bar_base_renderer = bars.add_glyph(bars_source,
+                                   Rect(x=0,
+                                        y='base_bins',
+                                        height=0.2,
+                                        width='base',
+                                        fill_color=BLUE,
+                                        fill_alpha=0.8,
+                                        line_color=None))
 
-bars.add_glyph(source,
-               Rect(x='x1',
-                    y='reform_bars',
+bars.add_glyph(bars_source,
+               Rect(x=0,
+                    y='reform_bins',
                     height=0.2,
-                    width=100,
+                    width='reform',
                     fill_color=RED,
                     fill_alpha=1,
                     line_color=None))
@@ -154,13 +148,16 @@ plots = {}
 plots['bars_plot'] = bars
 plots['line_plot'] = lines
 
-data = {}
-for k, v in data_sources.items():
-    data[k] = v.data
+bars_data = { k: v.data for k,v in bar_sources.items() }
+lines_data = { k: v.data for k,v in line_sources.items() }
 
 script, divs = components(plots)
 output_page('tax-reform-explorer.html',
             bokeh_script=script,
             plots=divs,
-            plot_id=lines._id,
-            bokeh_data=data)
+            line_plot_id=lines._id,
+            line_renderer_id=line_base_renderer._id,
+            bar_plot_id=bars._id,
+            bar_renderer_id=bar_base_renderer._id,
+            bars_data=bars_data,
+            lines_data=lines_data)
