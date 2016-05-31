b720b43a0db29f4dc906d4daa5e5ceed1a2cdf71
bcollins <bcollins@continuum.io>
2016-05-31 10:27:34 -0500

    added build-plots cli tool

--- taxplots/cli.py
+++ taxplots/cli.py
@@ -1,5 +1,12 @@
 from __future__ import print_function, division, absolute_import
-from os import path, listdir, environ
+
+from os import path, listdir, environ, system
+from pdb import set_trace
+
+import pandas as pd
+import yaml
+
+from taxplots.utils import ordered_load, ChDir
 
 def _validate_plot_dir(directory):
     pass
@@ -7,16 +14,64 @@ def _validate_plot_dir(directory):
 def _get_plots():
     pass
 
+def _get_plot_info(plot_dir):
+    plot_files = listdir(plot_dir)
+    if 'info.yml' in plot_files:
+        info_file = path.join(plot_dir, 'info.yml')
+        with open(info_file) as f:
+            info = ordered_load(f.read())
+            info['directory'] = plot_dir
+            return info
+
+def _run_plot(plot):
+    with ChDir(plot.directory) as cwd:
+        result = system(plot.build_cmd)
+    return result == 0
+
 def list_plots():
-    plots = path.join(path.split(__file__)[0], '../contrib')
-    print(plots)
-    return plots
+    plot_dir = path.abspath(path.join(path.split(__file__)[0], '../contrib'))
+
+    if not path.exists(plot_dir):
+        raise IOError("Can't find plot directory: {0}".format(plot_dir))
+
+    plots = [path.join(plot_dir, d) for d in listdir(plot_dir)]
+    infos = [_get_plot_info(p) for p in plots]
+    df = pd.DataFrame(infos)
+    return df
 
 def build_plots():
     print('building plots...')
-    pass
+    plots_df = list_plots()
+    plots_df['build_successful'] = plots_df.apply(_run_plot, axis=1)
+
+    # log successful builds
+    success_df = plots_df[plots_df['build_successful'] == True]
+    if not success_df.empty:
+        print('\n\nSuccessfully Built:')
+        print('-------------------')
+        print(success_df[['plot_name','plot_id','build_cmd']])
+        print('\n\n')
+
+    # log error builds
+    error_df = plots_df[plots_df['build_successful'] == False]
+    if not error_df.empty:
+        print('\n\nErrors while Building:')
+        print('----------------------')
+        print(error_df[['plot_name','plot_id','build_cmd']])
+        print('\n\n')
 
 def upload_plots():
     print('uploading plots...')
-    access_key = environ.get('')
+    access_key = environ.get('AWS_ACCESS_KEY')
     secret_key = environ.get('')
+    upload_bucket = environ.get('')
+
+    env_msg = 'Environment varibles not set {0}, try: export {0}=<some value>'
+
+    if not access_key:
+        raise ValueError(env_msg.format('AWS_ACCESS_KEY'))
+    if not secret_key:
+        raise ValueError(env_msg.format('AWS_SECRET_KEY'))
+    if not upload_bucket:
+        raise ValueError(env_msg.format('TAXPLOT_S3_BUCKET'))
+
---
+++ taxplots/utils.py
@@ -0,0 +1,28 @@
+import os
+import yaml
+from collections import OrderedDict
+
+def ordered_load(stream, Loader=yaml.Loader, object_pairs_hook=OrderedDict):
+    class OrderedLoader(Loader):
+        pass
+    def construct_mapping(loader, node):
+        loader.flatten_mapping(node)
+        return object_pairs_hook(loader.construct_pairs(node))
+    OrderedLoader.add_constructor(
+        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+        construct_mapping)
+    return yaml.load(stream, OrderedLoader)
+
+class ChDir(object):
+    """
+    Context Manager for changing directories
+    """
+    def __init__(self, path):
+        self.old_dir = os.getcwd()
+        self.new_dir = path
+ 
+    def __enter__(self):
+        os.chdir(self.new_dir)
+ 
+    def __exit__(self, *args):
+        os.chdir(self.old_dir)
