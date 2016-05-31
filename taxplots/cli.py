69e15f1d0ce7154a9ab50fbe4d3990cf71ce7734
bcollins <bcollins@continuum.io>
2016-05-31 11:54:00 -0500

    moved files

--- taxplots/cli.py
+++ taxplots/cli.py
@@ -5,6 +5,7 @@ from pdb import set_trace
 
 import pandas as pd
 import yaml
+import boto3
 
 from taxplots.utils import ordered_load, ChDir
 
@@ -28,6 +29,11 @@ def _run_plot(plot):
         result = system(plot.build_cmd)
     return result == 0
 
+def _upload_plot(bucket, plot):
+    with ChDir(plot.directory) as cwd:
+        result = system(plot.build_cmd)
+    return result == 0
+
 def list_plots():
     plot_dir = path.abspath(path.join(path.split(__file__)[0], '../contrib'))
 
@@ -40,7 +46,6 @@ def list_plots():
     return df
 
 def build_plots():
-    print('building plots...')
     plots_df = list_plots()
     plots_df['build_successful'] = plots_df.apply(_run_plot, axis=1)
 
@@ -62,16 +67,38 @@ def build_plots():
 
 def upload_plots():
     print('uploading plots...')
-    access_key = environ.get('AWS_ACCESS_KEY')
-    secret_key = environ.get('')
-    upload_bucket = environ.get('')
+    access_key = environ.get('AWS_KEY_ID')
+    secret_key = environ.get('AWS_SECRET_ID')
+    upload_bucket = environ.get('TAXPLOT_S3_BUCKET')
 
     env_msg = 'Environment varibles not set {0}, try: export {0}=<some value>'
 
     if not access_key:
-        raise ValueError(env_msg.format('AWS_ACCESS_KEY'))
+        raise ValueError(env_msg.format('AWS_KEY_ID'))
     if not secret_key:
-        raise ValueError(env_msg.format('AWS_SECRET_KEY'))
+        raise ValueError(env_msg.format('AWS_SECRET_ID'))
     if not upload_bucket:
         raise ValueError(env_msg.format('TAXPLOT_S3_BUCKET'))
+
+    s3 = boto3.resource('s3')
+    s3.meta.client.upload_file('/tmp/hello.txt', upload_bucket, 'hello.txt')
+
+    plots_df = list_plots()
+    plots_df['upload_successful'] = plots_df.apply(_upload_plot, axis=1)
+
+    # log successful uploads
+    success_df = plots_df[plots_df['build_successful'] == True]
+    if not success_df.empty:
+        print('\n\nSuccessfully Built:')
+        print('-------------------')
+        print(success_df[['plot_name','plot_id','build_cmd']])
+        print('\n\n')
+
+    # log error uploads
+    error_df = plots_df[plots_df['build_successful'] == False]
+    if not error_df.empty:
+        print('\n\nErrors while Building:')
+        print('----------------------')
+        print(error_df[['plot_name','plot_id','build_cmd']])
+        print('\n\n')
 
