175d90377b18261389a215a8c3f72d69653e1220
bcollins <bcollins@continuum.io>
2016-08-25 11:20:12 -0500

    added center line, added jinja template, layout tweaks

--- contrib/corp_metr/build.py
+++ contrib/corp_metr/build.py
@@ -3,14 +3,29 @@ This program reads in the btax output dataframes and plots the results.
 '''
 import pandas as pd
 import pickle
-from bokeh.layouts import row
+
+from os import path
+from math import pi
+
+from jinja2 import Environment, FileSystemLoader
 from bokeh.plotting import figure
 from bokeh.models import HoverTool
 from bokeh.models import ColumnDataSource
-from bokeh.io import output_file, show
+from bokeh.models import NumeralTickFormatter
+from bokeh.embed import components
+
+def output_page(**kwargs):
+    here = path.dirname(path.abspath(__file__))
+    j2_env = Environment(loader=FileSystemLoader(here), trim_blocks=True)
+    content = j2_env.get_template('template.j2').render(**kwargs)
+    with open('index.html', 'w') as output_file:
+        output_file.write(content)
 
 with open("resources/by_asset.pkl", "rb") as pick:
     df = pickle.load(pick)
+
+df = df[df['asset_category'] != 'Intellectual Property'].copy()
+df['metr_c_fmt'] = df.apply(lambda x: "{0:.1f}%".format(x['metr_c'] * 100), axis=1)
 
 asset_order = ['Computers and Software',
                'Instruments and Communications Equipment',
@@ -24,17 +39,24 @@ asset_order = ['Computers and Software',
                'Mining and Drilling Structures',
                'Other Structures']
 
-p = figure(plot_height=420,
-           plot_width=620,
-           title="Marginal Effective Tax Rates on Corporate Investments, 2016 Law",
+# reform and baseline policy as separate colors
+# use of pickles long term?
+# commiting large files to git...?
+
+p = figure(plot_height=380,
+           plot_width=990,
            y_range=list(reversed(asset_order)),
-           x_range=[-.15, .3],
-           tools='pan,box_zoom,reset,hover')
+           tools='hover')
 p.xaxis.axis_label = "Marginal Effective Tax Rate"
+p.xaxis[0].formatter = NumeralTickFormatter(format="0.1%")
 p.yaxis.axis_label = "Asset Category"
+p.yaxis.major_label_orientation = pi/4
+p.toolbar_location = None
+p.min_border_right = 5
+p.min_border_bottom = 60
 
 hover = p.select(dict(type=HoverTool))
-hover.tooltips = [('Asset', '@Asset')]
+hover.tooltips = [('Asset', ' @Asset (@metr_c_fmt)')]
 
 SIZES = list(range(6, 22, 3))
 df['size'] = pd.qcut(df['assets'].values, len(SIZES), labels=SIZES)
@@ -42,13 +64,17 @@ df['size'] = pd.qcut(df['assets'].values, len(SIZES), labels=SIZES)
 source = ColumnDataSource(df)
 p.circle(x='metr_c',
          y='asset_category',
-         source=source,
          color="#73000A",
          size='size',
          line_color="white",
-         alpha=.6,
-         hover_color='white',
-         hover_alpha=0.5)
-layout = row(p)
-output_file('index.html')
-show(layout)
+         source=source,
+         alpha=.6)
+
+p.line(x=[0,0],
+       y=[0,100],
+       line_dash=[5,5],
+       color='black')
+
+plots = dict(metr=p)
+script, divs = components(plots)
+output_page(bokeh_script=script, plots=divs)
