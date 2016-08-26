5bf2e894c7953a3f881372258af5e010786f6921
bcollins <bcollins@continuum.io>
2016-08-26 11:20:14 -0500

    wip

--- contrib/corp_metr/build.py
+++ contrib/corp_metr/build.py
@@ -24,8 +24,14 @@ def output_page(**kwargs):
 with open("resources/by_asset.pkl", "rb") as pick:
     df = pickle.load(pick)
 
+reform_df = pd.read_csv("resources/reform_byasset.csv")
+import pdb;pdb.set_trace()
+
 df = df[df['asset_category'] != 'Intellectual Property'].copy()
+reform_df = reform_df[reform_df['asset_category'] != 'Intellectual Property'].copy()
+
 df['metr_c_fmt'] = df.apply(lambda x: "{0:.1f}%".format(x['metr_c'] * 100), axis=1)
+reform_df['metr_c_fmt'] = reform_df.apply(lambda x: "{0:.1f}%".format(x['metr_c'] * 100), axis=1)
 
 asset_order = ['Computers and Software',
                'Instruments and Communications Equipment',
@@ -39,10 +45,6 @@ asset_order = ['Computers and Software',
                'Mining and Drilling Structures',
                'Other Structures']
 
-# reform and baseline policy as separate colors
-# use of pickles long term?
-# commiting large files to git...?
-
 p = figure(plot_height=380,
            plot_width=990,
            y_range=list(reversed(asset_order)),
@@ -50,25 +52,34 @@ p = figure(plot_height=380,
 p.xaxis.axis_label = "Marginal Effective Tax Rate"
 p.xaxis[0].formatter = NumeralTickFormatter(format="0.1%")
 p.yaxis.axis_label = "Asset Category"
-p.yaxis.major_label_orientation = pi/4
 p.toolbar_location = None
 p.min_border_right = 5
-p.min_border_bottom = 60
 
 hover = p.select(dict(type=HoverTool))
 hover.tooltips = [('Asset', ' @Asset (@metr_c_fmt)')]
 
 SIZES = list(range(6, 22, 3))
 df['size'] = pd.qcut(df['assets'].values, len(SIZES), labels=SIZES)
+reform_df['size']= pd.qcut(reform_df['assets_c'].values, len(SIZES), labels=SIZES)
 
 source = ColumnDataSource(df)
+reform_source = ColumnDataSource(reform_df)
+
 p.circle(x='metr_c',
          y='asset_category',
-         color="#73000A",
+         color="#900000",
          size='size',
          line_color="white",
          source=source,
-         alpha=.6)
+         alpha=.4)
+
+p.circle(x='metr_c',
+         y='asset_category',
+         color="#336699",
+         size='size',
+         line_color="white",
+         source=reform_source,
+         alpha=.4)
 
 p.line(x=[0,0],
        y=[0,100],
@@ -77,4 +88,6 @@ p.line(x=[0,0],
 
 plots = dict(metr=p)
 script, divs = components(plots)
-output_page(bokeh_script=script, plots=divs)
+output_page(bokeh_script=script,
+            
+            plots=divs)
