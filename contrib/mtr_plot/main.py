592988d634c515165c8ba21f9010890d44d4e694
Sean <zhwa@umich.edu>
2016-10-21 15:22:49 -0400

    styling

--- contrib/mtr_plot/main.py
+++ contrib/mtr_plot/main.py
@@ -1,14 +1,23 @@
 import pandas as pd
 import numpy as np
-from bokeh.models import (ColumnDataSource, LogAxis, LinearAxis, Rect, FactorRange,
-                          CategoricalAxis, Line, Text, Square, HoverTool)
+from styles import BLUE, RED, GREEN
+from bokeh.models import (ColumnDataSource, LogAxis, LinearAxis, Rect, FactorRange, BoxZoomTool, ResetTool,
+                          CategoricalAxis, Line, Text, Square, HoverTool, BoxSelectTool)
 from bokeh.models import Plot, Range1d, ImageURL, DataRange1d, Select
 from bokeh.embed import components
 from bokeh.models import CustomJS, Slider
 from bokeh.io import curdoc
 from bokeh.plotting import figure, hplot, vplot, output_file, show
-from bokeh.layouts import row, widgetbox
-from bokeh.palettes import Blues4, Reds4, Greens4
+from bokeh.layouts import row, widgetbox, column, gridplot
+
+hover = HoverTool(
+        tooltips=[
+            ("Marginal Tax Rate", "$y")
+        ]
+    )
+
+plot_width = 425
+
 
 def get_csv_data(MARS):
     source_csv = pd.read_csv('mtr_data.csv')
@@ -19,55 +28,65 @@ def get_csv_data(MARS):
         df = source_csv.iloc[:,[3,4,5]]
     elif MARS == 'Head of Household':
         df = source_csv.iloc[:,[6,7,8]]
-    df.dropna()
+    df = df.dropna()
     df.index = (df.reset_index()).index
     df.columns = ['base','reform','reform_2']
-    df['index'] = df.index
     return df
 
 
 def make_a_plot(source):
-    plot = figure(plot_height=600, plot_width=800,
-                  tools="")
-    plot.line('index','reform',source = source, line_color=Greens4[0], line_width=2, line_alpha=.8)
-    plot.line('index','reform_2',source = source, line_color=Reds4[0], line_width=2, line_alpha=.8)
-    plot.line('index','base',source = source, line_color=Blues4[0], line_width=2, line_alpha=1.2)
-    plot.yaxis.axis_label = 'Avg. MTR on Wage Income'
+    plot = figure(plot_height=425, plot_width=plot_width,
+                  title='Effect of Candidate\'s Proposals on Labor Incentives',
+                  tools=[hover, BoxZoomTool(), ResetTool()])
+
+    plot.title.align = 'center'
+    plot.title.text_font_size = '12pt'
+    plot.line('index','reform',source = source, line_color=GREEN, line_width=2, line_alpha=.8, legend="Trump")
+    plot.line('index','reform_2',source = source, line_color=RED, line_width=2, line_alpha=.8, legend="Hillary")
+    plot.line('index','base',source = source, line_color=BLUE, line_width=2, line_alpha=1, legend="Base")
+    plot.yaxis.axis_label = 'Average Marginal Tax Rate on Wage Income'
     plot.xaxis.axis_label = 'Percentile of Adjusted Gross Income'
+    plot.legend.location = "bottom_right"
+    plot.legend.label_text_font = "times"
+    plot.legend.label_text_font_style = "italic"
+    plot.legend.label_width = 5
+    plot.legend.label_height = 5
+    plot.legend.label_standoff = 5
+    plot.legend.glyph_width = 20
+    plot.legend.glyph_height = 20
+    plot.legend.legend_spacing = 5
+    plot.legend.legend_padding = 5
     return plot
 
+
+options = ['Single', 'Joint', 'Head of Household']
+scr1 = ColumnDataSource(get_csv_data(options[0]))
+scr2 = ColumnDataSource(get_csv_data(options[1]))
+scr3 = ColumnDataSource(get_csv_data(options[2]))
 ref_source = ColumnDataSource(dict(
                     index = [],
                     base = [],
                     reform = [],
                     reform_2 = []
                 ))
-
-mars = 'Single'
-options = ['Single', 'Joint', 'Head of Household']
-scr1 = ColumnDataSource(get_csv_data(options[0]))
-scr2 = ColumnDataSource(get_csv_data(options[1]))
-scr3 = ColumnDataSource(get_csv_data(options[2]))
-
+sources = dict(Single=scr1, Joint=scr2, Head=scr3)
 plot = make_a_plot(ref_source)
-callback = CustomJS(args=dict(sin_ = scr1, jon_ = scr2, sep_ = scr3 ), code="""
+callback = CustomJS(args=sources, code="""
         var input = select.value;
-        var data_1 = sin_.data;
-        var data_2 = jon_.data;
-        var data_3 = sep_.data;
+        var data_1 = Single.data;
+        var data_2 = Joint.data;
+        var data_3 = Head.data;
         if (input == "Single"){
             ref_source.data = data_1;
         } else if (input == "Joint"){
             ref_source.data = data_2;
-        } else{
+        } else if (input == "Head of Household"){
             ref_source.data = data_3;
         }
         ref_source.trigger('change');
     """)
-select = Select(value=mars, title='MARS', options=options,  callback=callback)
+select = Select(title='Filing Status', options=options,  callback=callback)
 callback.args['select'] = select
 callback.args['ref_source'] = ref_source
-
-layout = row(plot, select)
-
-show(layout)
+grid = column(select, plot)
+show(grid)
